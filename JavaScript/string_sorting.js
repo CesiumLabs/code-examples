bd17f5c34dadb5b8ffa504ed3a40e7f3e94f4588
Takiyo <alanwalkerlol049@gmail.com>
2021-10-01 19:54:29 +0700

    Create string_sorting.js

---
+++ JavaScript/string_sorting.js
@@ -0,0 +1,20 @@
+const strings = [
+    `Hello world`,
+    `I'm cool`,
+    `You are so handsome`,
+    `Moyai`,
+    `Mice love cheese`
+]
+
+const sorted = strings.sort((a, b) => {
+    return (a > b) ? 1 : ((b > a) ? -1 : 0)
+})
+
+console.log(sorted)
+// [
+//   'Hello world',
+//   "I'm cool",
+//   'Mice love cheese',
+//   'Moyai',
+//   'You are so handsome'
+// ]
