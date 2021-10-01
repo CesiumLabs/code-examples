4f53c38ee640afdb1c141e6c51bdc2bd8f18382c
Rohan Kumar <rohan.shuvam@gmail.com>
2021-10-01 10:36:38 +0530

    Added Java, php and C#

---
+++ c#/helloworld.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorld{
+    class hello{
+        static void Main(strin[] args){
+            Console.WriteLine("Hello World");
+            console.ReadKey();
+            console.ReadKey();
+        }
+    }
+}
