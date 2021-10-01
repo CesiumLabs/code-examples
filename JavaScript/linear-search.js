950bad8786718f0689a09eed289119d3388ce6e0
HyRo <generalkubo@gmail.com>
2021-10-01 18:19:12 +0200

    refactor(JavaScript/linear-search): use built-in find

--- JavaScript/linear-search.js
+++ JavaScript/linear-search.js
@@ -2,11 +2,11 @@ const numbers = [15, 2, 1, 8, 10, 5, 20, 25];
 const numberSought = 8;
 
 const linearSearch = (numbers, wanted) => {
-    for (const number of numbers) 
-        if (number === wanted) return true;
+    let find = numbers.find(n => n === wanted);
+    if(find) return true;
 
     return false;
 }
 
 
-linearSearch(numbers, numberSought); 
+linearSearch(numbers, numberSought);
