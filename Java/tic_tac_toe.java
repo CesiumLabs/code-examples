a89b26bc72e0cd12e2bd7f4eb088ab7f72191ffa
null <60427892+vierofernando@users.noreply.github.com>
2021-10-01 20:50:30 +0700

    refactor(

--- Java/tic_tac_toe.java
+++ Java/tic_tac_toe.java
@@ -1,50 +1,46 @@
 import java.util.Scanner;
 
 public class tic_tac_toe {
-
     int hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9;
 
     public static void main(String[] args) {
         tic_tac_toe thisClass = new tic_tac_toe();
         var scanner = new Scanner(System.in);
         System.out.println("|       |       |       |");
-
         System.out.println("| hole1 | hole2 | hole3 |");
         System.out.println("-------------------------");
         System.out.println("| hole4 | hole5 | hole6 |");
         System.out.println("-------------------------");
         System.out.println("| hole7 | hole8 | hole9 |");
-
         System.out.println("|       |       |       |");
+        
         boolean userXTurn = true;
-        for (var i = 0; i <= 9; i++) {
+        
+        for (int i = 0; i <= 9; i++) {
             System.out.println((userXTurn ? "X's" : "O's") + " turn");
             int input = scanner.nextInt();
             if (input <= 9 && input >= 1) {
                 boolean filled = thisClass.fillHole(input, userXTurn ? 1 : 2);
                 if (filled) {
-                    thisClass.PrintCurrentState();
+                    this.PrintCurrentState();
                     userXTurn = !userXTurn;
                     int userWon = thisClass.checkIfWon();
+                    
                     if (userWon > 0) {
                         System.out.println("Congrats " + thisClass.getPlayerChar(userWon) + " for winning the game!");
                         return;
-
-                    } else {
-                        if (i == 8) {
-                            System.out.println("No One Won ! Good Luck next time!");
-                            return;
-                        }
+                    } else if (i == 8) {
+                        System.out.println("No One Won ! Good Luck next time!");
+                        return;
                     }
                 } else {
                     System.out.println("This hole is already filled!");
                     i--;
                     thisClass.PrintCurrentState();
                 }
-            } else {
-                System.out.println("You have entered a number out of range please enter a number from 1 to 9");
-                i--; // decreasing i so the i ++ dosent affect this chance
             }
+            System.out.println("You have entered a number out of range please enter a number from 1 to 9");
+            i--; // decreasing i so the i ++ dosent affect this chance
         }
     }
 
@@ -70,12 +66,11 @@ public class tic_tac_toe {
             return hole9;
         } else if ((hole3 == hole5) && (hole5 == hole7) && (hole7 > 0)) {
             return hole7;
-        } else
-            return 0;
+        }
+        return 0;
     }
 
     private void PrintCurrentState() {
-
         System.out.println("| " + (hole1 > 0 ? this.getPlayerChar(hole1) : "hole1") + " | "
                 + (hole2 > 0 ? this.getPlayerChar(hole2) : "hole2") + " | "
                 + (hole3 > 0 ? this.getPlayerChar(hole3) : "hole3") + " |");
@@ -90,63 +85,35 @@ public class tic_tac_toe {
     }
 
     private boolean fillHole(int holeNumber, int player) {
-        if (holeNumber == 1) {
-            if (hole1 == 0) {
-                hole1 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 2) {
-            if (hole2 == 0) {
-                hole2 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 3) {
-            if (hole3 == 0) {
-                hole3 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 4) {
-            if (hole4 == 0) {
-                hole4 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 5) {
-            if (hole5 == 0) {
-                hole5 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 6) {
-            if (hole6 == 0) {
-                hole6 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 7) {
-            if (hole7 == 0) {
-                hole7 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 8) {
-            if (hole8 == 0) {
-                hole8 = player;
-                return true;
-            } else
-                return false;
-        } else if (holeNumber == 9) {
-            if (hole9 == 0) {
-                hole9 = player;
-                return true;
-            } else
-                return false;
-        } else {
-            return false;
+        if (holeNumber == 1 && hole1 == 0) {
+            hole1 = player;
+            return true;
+        } else if (holeNumber == 2 && hole2 == 0) {
+            hole2 = player;
+            return true;
+        } else if (holeNumber == 3 && hole3 == 0) {
+            hole3 = player;
+            return true;
+        } else if (holeNumber == 4 && hole4 == 0) {
+            hole4 = player;
+            return true;
+        } else if (holeNumber == 5 && hole5 == 0) {
+            hole5 = player;
+            return true;
+        } else if (holeNumber == 6 && hole6 == 0) {
+            hole6 = player;
+            return true;
+        } else if (holeNumber == 7 && hole7 == 0) {
+            hole7 = player;
+            return true;
+        } else if (holeNumber == 8 && hole8 == 0) {
+            hole8 = player;
+            return true;
+        } else if (holeNumber == 9 && hole9 == 0) {
+            hole9 = player;
+            return true;
         }
+        return false;
     }
 
 }
