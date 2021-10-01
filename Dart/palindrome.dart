f0dfeda52b2749f0531156eaddf2da5aac948256
ZYROUGE <zyrouge@hotmail.com>
2021-10-01 10:39:02 +0530

    refactor(Dart): dont exit on empty input

--- Dart/palindrome.dart
+++ Dart/palindrome.dart
@@ -10,13 +10,12 @@ void main() {
     final String? input = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
     if (input == null || input.isEmpty) {
       print('Inputed received no value :(');
-      return;
+    } else {
+      final String reversed = input.split('').reversed.join('');
+      print('Input: $input');
+      print('Reversed: $reversed');
+      print('Is a palindrome: ${input == reversed ? 'Yes' : 'No'}');
     }
-
-    final String reversed = input.split('').reversed.join('');
-    print('Input: $input');
-    print('Reversed: $reversed');
-    print('Is a palindrome: ${input == reversed ? 'Yes' : 'No'}');
     
     stdout.write('\nDo you want to continue? (Y/N): ');
     next = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!)?.toLowerCase() == 'y';
