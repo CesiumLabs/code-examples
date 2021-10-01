fb0d7009ff0f700fc119de0d713c2f3aba11df84
null <60427892+vierofernando@users.noreply.github.com>
2021-10-01 23:29:18 +0700

    refactor(JavaScript/linear-search.js): turn linearSearch to a one-liner

--- JavaScript/linear-search.js
+++ JavaScript/linear-search.js
@@ -1,12 +1,5 @@
 const numbers = [15, 2, 1, 8, 10, 5, 20, 25];
 const numberSought = 8;
 
-const linearSearch = (numbers, wanted) => {
-    let find = numbers.find(n => n === wanted);
-    if(find) return true;
-
-    return false;
-}
-
-
+const linearSearch = (numbers, wanted) => !!numbers.find(n => n === wanted);
 linearSearch(numbers, numberSought);
