725ae4671aeffdc2bd3e1293efd6b68b68c72a18
HyRo <generalkubo@gmail.com>
2021-10-01 17:07:55 +0200

    Update ping.js

--- Nodejs/DiscordJS/gcommands/commands/ping.js
+++ Nodejs/DiscordJS/gcommands/commands/ping.js
@@ -5,6 +5,7 @@ module.exports = class extends Command {
     super(client, {
       name: 'ping',
       description: 'Ping commnd!',
+      context: "false"
     });
   }
   run({ respond, client }) {
