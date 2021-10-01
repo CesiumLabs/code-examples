e2c672c2ddbef05b078f7b59ff15e192446c1ff7
Whirl <ninja21official@gmail.com>
2021-10-01 10:31:19 +0530

    added c calc , js login , py random reddit

---
+++ Python/random-post-from-subreddits.py
@@ -0,0 +1,6 @@
+import random
+import requests
+Rads = random.choice([memes , neko , movies])
+ response = requests.get("https://www.reddit.com/r/${Rads}/random/.json")
+ result = response.json()
+ print(result) # you can do whatever you want with these values now , and example ( how to get the link of the post) : result['url]
