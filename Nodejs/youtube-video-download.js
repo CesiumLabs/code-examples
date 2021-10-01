2d75e49072d78df7d854589d626c1ea27e3aabb4
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-10-01 22:58:37 +0545

    feat(Nodejs): add youtube video downloading example

---
+++ Nodejs/youtube-video-download.js
@@ -0,0 +1,11 @@
+// npm install ytdl-core
+const ytdl = require("ytdl-core");
+const fs = require("node:fs");
+
+const url = "YOUTUBE_VIDEO_URL";
+const youtubeStream = ytdl(url);
+const writable = youtubeStream.pipe(fs.createWriteStream("./video.mp4"));
+
+writable.on("finish", () => {
+    console.log("Download complete!");
+});
