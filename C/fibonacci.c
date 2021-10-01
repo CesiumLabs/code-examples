a09f3829bcf0669b981e4f4cbfa0b05f9c3c3b95
Aashutosh Sapkota <55585284+Aashutoshbro@users.noreply.github.com>
2021-10-01 13:38:55 +0545

    Update fibonacci.c
    i have added some beautiful frontend and full stop at the end which seems to be great

--- C/fibonacci.c
+++ C/fibonacci.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main() {
-    int a = 0, b = 1, c = 0, terms;
 
-    printf("\nEnter the number of terms: ");
-    scanf("%d", &terms);
+  int i, n;
+  int n1 = 0, n2 = 1;
+  int nxtNumber = n1 + n2;
+  printf("Enter the number of terms: ");
+  scanf("%d", &n);
 
-    for (int i = 0; i < terms; i++) {
-        c = a + b;
-        b = a;
-        a = c;
+  printf("Fibonacci Series: %d, %d, ", n1, n2);
 
-        printf("\n%d", b);
-    }
+  for (i = 3; i <= n; ++i) {
+    printf("%d, ", nxtNumber);
+    n1 = n2;
+    n2 = nxtNumber;
+    nxtNumber = n1 + n2;
+  }
+   printf("%d.",nxtNumber);
+   printf("\nThank you for your trying our feature.");
+ 
 
-    return 0;
+  return 0;
 }
