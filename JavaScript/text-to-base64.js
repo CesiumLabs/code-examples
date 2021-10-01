8841eee5f79e1319933bcc03d81bb93409702b8b
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-10-01 22:56:10 +0545

    feat(JavaScript): add base64 example

---
+++ JavaScript/text-to-base64.js
@@ -0,0 +1,15 @@
+function toText(base64) {
+    return atob(base64);
+}
+
+function toBase64(text) {
+    return btoa(text);
+}
+
+const text = prompt("Enter a text:", ""); // Hello World
+const base64 = toBase64(text);
+const normalized = toText(base64);
+
+console.log(`Original: ${text}`); // -> Original: Hello World
+console.log(`Base64: ${base64}`); // -> Base64: SGVsbG8gV29ybGQ=
+console.log(`Normalized: ${normalized}`); // -> Normalized: Hello World
