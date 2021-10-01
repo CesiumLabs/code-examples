565cbb9f7c274a66aea4516afeb466019f844e94
Takiyo <mail@chilledchino.live>
2021-10-01 20:45:59 +0700

    add fastify-hello-world

---
+++ Nodejs/fastify-hello-world/index.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')({
+    logger: true
+})
+
+fastify.get('/',  (req, rep) => {
+    rep.type('application/json').code(200)
+    return { message: 'Hello world!' }
+})
+
+fastify.listen(3000, (err, address) => {
+    if (err) throw err
+    console.log(`Now listening to ${address}`)
+})
