35f1a693347b4360608efd4906c670899ffd86d8
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2021-10-01 23:46:49 +0530

    Element that appears once
    The following program finds out the elements that appears once in a sorted array. It follows a time complexity of O(log(n))

---
+++ C++/Element That Appears once.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int search(int A[], int N){
+	    //code
+	    int res=0;
+	    for(int i=0;i<N;i++){
+	        res = res^A[i];
+	    }
+	    return res;
+	}
+
+};
+
+// { Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t,len;
+    cin>>t;
+    while(t--)
+    {
+        cin>>len;
+        int arr[len];
+        for(int i=0;i<len;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.search(arr, len)<<'\n';
+    }
+    return 0;
+}
+  // } Driver Code Ends
