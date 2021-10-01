a2c2907d455a277e4716d3c86f908ed81fb6834f
tj <rhydderchc@gmail.com>
2021-10-01 11:47:05 -0500

    (Refactor): Used `some` instead of `find`

--- JavaScript/linear-search.js
+++ JavaScript/linear-search.js
@@ -1,5 +1,5 @@
 const numbers = [15, 2, 1, 8, 10, 5, 20, 25];
-const numberSought = 8;
+const numberSought = 15;
 
-const linearSearch = (numbers, wanted) => !!numbers.find(n => n === wanted);
-linearSearch(numbers, numberSought);
+const linearSearch = (numbers, wanted) => numbers.some(e => e == numberSought)
+linearSearch(numbers, numberSought)
