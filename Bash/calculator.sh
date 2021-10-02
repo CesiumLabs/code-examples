7ce4b4052cd5121a2a9f839dd59ad281e6bf1d2c
Souvik Mandal <souvikmandal210@gmail.com>
2021-10-02 22:33:17 +0530

    added programs using bash script

---
+++ Bash/calculator.sh
@@ -0,0 +1,58 @@
+# simple arithmatic calculator using bash scripting
+
+### run this file by executing the commmand>>    bash calculator.sh -o '<operator>' -a <first number> -b <second number>
+
+# flag 'o' is to choose operator among + - * /
+# flag 'a' is to enter first integer value
+# flag 'b' is to choose second integer value
+
+# example:-
+#
+# input:-
+#
+# ./calculator.sh -o '-' -a 20 -b 5
+#
+# output:-
+#
+# 20 - 5 = 15
+#
+# operation completed!
+#
+#######################################################################################################################################
+
+
+
+while getopts o:a:b: flag
+do
+    case "${flag}" in
+        o) operation=${OPTARG};;
+        a) a=${OPTARG};;
+        b) b=${OPTARG};;
+    esac
+done
+
+echo;
+
+if [ $operation == '+' ];  
+then  
+   c=$(( $a + $b )); 
+   echo "$a + $b = $c";
+elif [ $operation == '-' ];  
+then 
+  c=$(( $a - $b ));
+  echo "$a - $b = $c";
+elif [ $operation == '*' ];  
+then 
+  c=$(( $a * $b ));
+  echo "$a * $b = $c";
+elif [ $operation == '/' ];  
+then 
+  c=$(( $a / $b ));
+  echo "$a / $b = $c";
+  else
+  echo "invalid operation"
+  exit 1
+fi;
+
+echo;
+echo "operation completed!";
---
+++ Bash/hello.sh
@@ -0,0 +1,6 @@
+# simple program to print string using bash scripting
+
+### run this file by executing the commmand>>   bash hello.sh
+
+
+echo "hello world!"
