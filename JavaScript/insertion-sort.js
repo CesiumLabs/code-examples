433b45d9e7daab0f68f6075a11a7d04b5fe3fccc
fatik07 <fatichur.r07@gmail.com>
2021-10-02 11:59:18 +0700

    add insertion sort in js

---
+++ JavaScript/insertion-sort.js
@@ -0,0 +1,14 @@
+// insertion sort
+const insertion_Sort = (nums) => {
+  for (let i = 1; i < nums.length; i++) {
+    let j = i - 1
+    let temp = nums[i]
+    while (j >= 0 && nums[j] > temp) {
+      nums[j + 1] = nums[j]
+      j--
+    }
+    nums[j+1] = temp
+  }
+  return nums
+}
+console.log(insertion_Sort([5, 2, 6, 3, -1, 4, 10]));
