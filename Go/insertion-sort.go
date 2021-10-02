c3e0c459d09bbb9917deba8cf05f9655bcff4ebe
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-10-02 14:51:54 +0530

    feat: minor change

--- Go/insertion-sort.go
+++ Go/insertion-sort.go
@@ -24,16 +24,16 @@ func generateSlice(size int) []int {
 	}
 	return slice
 }
- 
+
 func insertionsort(items []int) {
-    var n = len(items)
-    for i := 1; i < n; i++ {
-        j := i
-        for j > 0 {
-            if items[j-1] > items[j] {
-                items[j-1], items[j] = items[j], items[j-1]
-            }
-            j = j - 1
-        }
-    }
+	var n = len(items)
+	for i := 1; i < n; i++ {
+		j := i
+		for j > 0 {
+			if items[j-1] > items[j] {
+				items[j-1], items[j] = items[j], items[j-1]
+			}
+			j = j - 1
+		}
+	}
 }
--- Go/simple-server.go
+++ Go/simple-server.go
@@ -10,7 +10,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
+
 	log.Print("Server is up and running")
 	log.Fatal(http.ListenAndServe(":8081", nil))
-
 }
