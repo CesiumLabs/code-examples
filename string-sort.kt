b3126ac1d28af5eedc4a9ae4daa4f34cce9a14a0
HyRo <generalkubo@gmail.com>
2021-10-02 07:45:17 +0200

    Create string-sort.kt

---
+++ string-sort.kt
@@ -0,0 +1,8 @@
+import java.util.*
+
+fun main() {
+    val strings = arrayOf("ahoj","hello","house","you","discord","GitHub","hacktoberfest")
+    
+    strings.sort()
+    println("${Arrays.toString(strings)}")
+}
