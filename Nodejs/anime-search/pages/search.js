7fb3fd0b77190fb3cf04cd99f35dfc122f78d082
shisui <gwk24yt@gmail.com>
2021-10-02 22:14:27 +0530

    search 🔎 card added ➕

--- Nodejs/anime-search/pages/search.js
+++ Nodejs/anime-search/pages/search.js
@@ -7,9 +7,54 @@ function Search() {
 		<div>
 			<Head>
 				<title>{`Search Result: ${route.query.q} - The Anime Search`}</title>
+				<link rel="stylesheet" href="search.css" />
 			</Head>
 			<main>
-				<h1> Search </h1>
+				<h3> Results </h3>
+				<div class="card_container">
+					<div class="card">
+						<a href="url">
+							<img src="https://cdn.discordapp.com/attachments/880732844220100608/893753030489550869/logo_sugoi.jpg"
+								class="card_image" alt="image" />
+							<h3 class="card_title">title</h3>
+						</a>
+					</div>
+					<div class="card">
+						<a href="url">
+							<img src="https://cdn.discordapp.com/attachments/880732844220100608/893753030489550869/logo_sugoi.jpg"
+								class="card_image" alt="image" />
+							<h3 class="card_title">title</h3>
+						</a>
+					</div>
+					<div class="card">
+						<a href="url">
+							<img src="https://cdn.discordapp.com/attachments/880732844220100608/893753030489550869/logo_sugoi.jpg"
+								class="card_image" alt="image" />
+							<h3 class="card_title">title lorem</h3>
+						</a>
+					</div>
+					<div class="card">
+						<a href="url">
+							<img src="https://cdn.discordapp.com/attachments/880732844220100608/893753030489550869/logo_sugoi.jpg"
+								class="card_image" alt="image" />
+							<h3 class="card_title">title</h3>
+						</a>
+					</div>
+					<div class="card">
+						<a href="url">
+							<img src="https://cdn.discordapp.com/attachments/880732844220100608/893753030489550869/logo_sugoi.jpg"
+								class="card_image" alt="image" />
+							<h3 class="card_title">title</h3>
+						</a>
+					</div>
+					<div class="card">
+						<a href="url">
+							<img src="https://cdn.discordapp.com/attachments/880732844220100608/893753030489550869/logo_sugoi.jpg"
+								class="card_image" alt="image" />
+							<h3 class="card_title">title</h3>
+						</a>
+					</div>
+				</div>
 			</main>
 		</div>
 	);
