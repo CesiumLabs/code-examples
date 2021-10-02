3785c9d7dfb59997e8a28bc2d4936630696cb120
HyRo <generalkubo@gmail.com>
2021-10-02 07:19:28 +0200

    Update number-sort.cs

--- C#/number-sort.cs
+++ C#/number-sort.cs
@@ -1,8 +1,10 @@
+use System;
+
 public class Sort
 {
     public static void Main()
     {
-        int[] myArr= new int[5] {1,9,728,320,123,845,21};
+        int[] myArr= new int[7] {1,9,728,320,123,845,21};
         Array.Sort(myArr);
 
         foreach (int val in myArr)
