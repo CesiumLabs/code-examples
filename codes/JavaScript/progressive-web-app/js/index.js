2a6c308d921c8c5f779026cb3a8b1153395289a4
rhydderchc <rhydderchc@gmail.com>
2021-10-20 04:51:10 -0500

    fixed bug & prettier

--- codes/JavaScript/progressive-web-app/js/index.js
+++ codes/JavaScript/progressive-web-app/js/index.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
 	navigator.serviceWorker
 		.register("sw.js")
-		.then(registration => console.log(`SW Registered!\n${registration}`)
-		.catch(error => console.log(`SW Failed to register!\n${error}`));
+		.then((registration) => console.log(`SW Registered!\n${registration}`))
+		.catch((error) => console.log(`SW Failed to register!\n${error}`));
 }
