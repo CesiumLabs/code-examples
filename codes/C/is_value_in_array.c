63c88c8871e52d95c49ceffecead4eb58eb62e37
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-11-14 09:15:29 +0100

    Check if a value is in an array in C

---
+++ codes/C/is_value_in_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int value_in_array(float val, float arr[], float length) {
+    for(int i = 0; i < length; i++) {
+        if(arr[i] == val) {
+            return 1;
+		}
+    }
+    return 0;
+}
+
+int main() {
+	float arr1[] = {10, 5, 8};
+	float arr2[] = {13, 1, 7};
+	printf("%d\n", value_in_array(5, arr1, (int)(sizeof(arr1)/sizeof(arr1[0]))));
+	printf("%d", value_in_array(5, arr2, (int)(sizeof(arr2)/sizeof(arr2[0]))));
+}
