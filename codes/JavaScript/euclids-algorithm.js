2f19206d1f7eb820b1ce83a693880240a7a6255e
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-11-20 20:27:15 +0100

    feat(JS): Euclid's algorithm

---
+++ codes/JavaScript/euclids-algorithm.js
@@ -0,0 +1,27 @@
+/**
+ * Calculates the greatest common divisor of 2 numbers using the Euclid's algorithm
+ * @param {number} a first number
+ * @param {number} b second number
+ * @returns {number} the greatest common divisor
+ */
+function euclid(a, b) {
+	a = Math.abs(a);
+	b = Math.abs(b);
+	if (a < b) {
+		let holder = a;
+		a = b;
+		b = holder;
+	}
+	let r = 0;
+	while (b > 0) {
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+console.log(euclid(15, 20)); //5
+console.log(euclid(999, 837)); //27
+console.log(euclid(610, 987)); //1
+console.log(euclid(-999, -837)); //27
