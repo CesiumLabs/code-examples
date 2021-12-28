e5f01a25e2d3e57f69f81dd2cfc2582878d071e5
tj <rhydderchc@gmail.com>
2021-12-28 08:48:06 -0600

    feat: formatting

--- codes/C++/Bubble_Sort.cpp
+++ codes/C++/Bubble_Sort.cpp
@@ -1,39 +1,33 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-void bubbleSort(vector<int>& a, int n)
-{
-    for(int i=0;i<n-1;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        {
-            if(a[j] < a[i])
-            {
-                int temp =a[j];
-                a[j] =a[i];
-                a[i] =temp;
-            }
-        }
+void bubbleSort(vector < int > & a, int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (a[j] < a[i]) {
+        int temp = a[j];
+        a[j] = a[i];
+        a[i] = temp;
+      }
     }
+  }
 
 }
-int main()
-{
-    int n;
-    cin>>n;
-    vector<int> a;
-    for(int i=0;i<n;i++)
-    {
-        int x;
-        cin>>x;
-        a.push_back(x);
-    }
-    bubbleSort(a,n);
-    for(int i=0;i<n;i++)
-    {
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
-    return 0;
+int main() {
+  int n;
+  cin >> n;
+  vector < int > a;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    a.push_back(x);
+  }
+  bubbleSort(a, n);
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+  return 0;
 }
 
 /*OUTPUT
